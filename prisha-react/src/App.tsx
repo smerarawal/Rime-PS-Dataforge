bcff037dbed54f99d7c4c9e96fa820b2e7b5af9d
prishaa agrawal <prishaagrawal1702@gmail.com>
2026-09-07 00:58:37 +0530

    Add Prisha observability dashboard

--- prisha-react/src/App.tsx
+++ prisha-react/src/App.tsx
@@ -1,33 +1,7 @@
-import ObservabilityPanel from "./ObservabilityPanel";
-import "./App.css";
+import DashboardPage from "./DashboardPage";
 
 function App() {
-  return (
-    <main
-      style={{
-        minHeight: "100vh",
-        padding: "32px",
-        background: "#0d0d0d",
-        color: "white",
-        boxSizing: "border-box",
-      }}
-    >
-      <div
-        style={{
-          maxWidth: "1100px",
-          margin: "0 auto",
-        }}
-      >
-        <h1>Voice Agent Observability</h1>
-
-        <p style={{ color: "#aaa", marginBottom: "24px" }}>
-          Realtime status, request timeline and interruption metrics
-        </p>
-
-        <ObservabilityPanel />
-      </div>
-    </main>
-  );
+  return <DashboardPage />;
 }
 
 export default App;
